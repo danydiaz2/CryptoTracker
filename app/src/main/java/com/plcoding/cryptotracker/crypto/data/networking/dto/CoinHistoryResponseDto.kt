3345d855661ee2aa85a1691c0d0ddedfb0cc4ee4
Daniel Diaz <danydiaz788@gmail.com>
2025-01-06 21:57:30 -0500

    Add the line chart on Detail screen

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/dto/CoinHistoryResponseDto.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/dto/CoinHistoryResponseDto.kt
@@ -1,5 +1,8 @@
 package com.plcoding.cryptotracker.crypto.data.networking.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CoinHistoryResponseDto(
     val data: List<CoinPriceDto>,
 )
